b0588e1707cc35218dfc9e4838dfdb6841ccc9f9
Brian <130288605+brian3154@users.noreply.github.com>
2026-09-24 16:15:24 +0900

    Refactor script.js for improved readability and structure

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function newQuestion() {
   num2 = range[num1];
   display_hanja = allData[num2].shape;
   if (allData[num2].shape.length >= 4) {
-    hanja.style.fontSize = "5px";
+    hanja.style.fontSize = "20px";
   }
   if (allData[num2].mean == "") {
     document.getElementById("mean").disabled = true;
