4bdd32d0cff76dbba092ea9777be2bafc7df066f
Brian <130288605+brian3154@users.noreply.github.com>
2026-09-24 16:46:51 +0900

    Refactor event listeners and game logic
    Updated event listeners and logic for handling user input and displaying results.

--- script.js
+++ script.js
@@ -31,6 +31,11 @@ function newQuestion() {
   num1 = rand(0, range.length - 1);
   num2 = range[num1];
   display_hanja = allData[num2].shape;
+  if (display_hanja.length >= 4) {
+    hanja.style.fontSize = "100px";
+  } else {
+    hanja.style.fontSize = "200px";
+  }
   if (allData[num2].mean == "") {
     document.getElementById("mean").disabled = true;
   }
